5dec3d4f41edecf82a7398f41a0638df662237fe
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-04-07 15:16:11 +0000

    form update

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-  myFunction();
-};
+// window.onscroll = function () {
+//   myFunction();
+// };
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("prog-bar").style.width = scrolled + "%";
-}
+// function myFunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height =
+//     document.documentElement.scrollHeight -
+//     document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("prog-bar").style.width = scrolled + "%";
+// }
+
+
